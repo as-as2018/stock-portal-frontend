876712e4dd323ac72560187c8ba7e9719b7de834
as-as2018 <vksriwash002@gmail.com>
2025-04-30 17:44:22 +0530

    a

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const base_Url=true?'http://localhost:5000':'https://stock-trading-portal-dlx2.onrender.com'
+export const base_Url=false?'http://localhost:5000':'https://stock-trading-portal-dlx2.onrender.com'
 
